c4758afcc145c5a8bc89d8ee2fc1b8278fc4df72
Jairo Blatt <45108346+jairoblatt@users.noreply.github.com>
2020-07-25 18:06:48 -0300

    Update Overview.vue

--- src/components/Card/Overview.vue
+++ src/components/Card/Overview.vue
@@ -1,10 +1,7 @@
 <template>
   <div :class="`movie__desc ${details?'movie__desc__details':''}`">
-    <p
-      class="text"
-      v-show="overview"
-    >{{details? overview : overview.substring(0,170)}} </p>
-    <span v-if="!details" class="read__more"> Mais informações</span>
+    <p class="text" v-show="overview">{{details? overview : text}}</p>
+    <span v-if="!details" class="read__more">Mais informações</span>
   </div>
 </template>
 
@@ -18,6 +15,16 @@ export default {
     details: {
       type: Boolean,
       default: false,
+    },
+  },
+
+  computed: {
+    text() {
+      let overview = this.overview;
+      if (this.overview && overview.length >= 170) {
+        return `${overview.substring(0, 170)} ...`;
+      }
+      return overview;
     },
   },
 };
